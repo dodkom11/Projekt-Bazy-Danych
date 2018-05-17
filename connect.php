277d33641dbce89192fa920f228717e21d5b7b5a
dodkom11 <37537775+dodkom11@users.noreply.github.com>
2018-05-17 23:35:46 +0200

    baza update

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 	/*
 		INFORMACJE POTRZEBNE DO POŁĄCZENIA Z BAZĄ DANYCH
 	*/
-	$username = "sklep";          // username
-	$password = "pass";           // password
+	$username = "en1ceka";          // username
+	$password = "qwerty";           // password
 	$database = "localhost/XE";   // and the connect string to connect to your database
 ?>
